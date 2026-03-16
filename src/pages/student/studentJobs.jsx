ef892acf0b7c121d5796f67b5ee808a1aa3c4642
tharumaxmusicingithub <it23369924@my.sliit.lk>
2026-03-16 15:54:31 +0530

    Change the error file

--- src/pages/student/studentJobs.jsx
+++ src/pages/student/studentJobs.jsx
@@ -2,6 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import { Link } from "react-router-dom";
 import { jobsApi } from "../../services/jobs";
 
+
 const PAGE_SIZE = 6;
 
 export default function StudentJobs() {
